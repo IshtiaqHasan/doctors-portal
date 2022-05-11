b91586816e5eafd79915020e5d4ad98cc2ef6765
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-05-11 16:22:00 +0600

    bg added

--- src/Pages/Appointment/AppointmentBanner.js
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import bg from '../../assets/images/bg.png';
 import chair from '../../assets/images/chair.png';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css';
@@ -6,7 +7,12 @@ import 'react-day-picker/dist/style.css';
 const AppointmentBanner = ({ date, setDate }) => {
 
     return (
-        <div class="hero min-h-screen">
+        <div
+            style={{
+                background: `url(${bg})`,
+                backgroundSize: 'cover'
+            }}
+            class="hero min-h-screen">
             <div class="hero-content flex-col lg:flex-row-reverse">
                 <img src={chair} alt="Dentist Chair" class="max-w-sm rounded-lg shadow-2xl" />
                 <div className='m-20'>
--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -4,8 +4,8 @@ const Service = ({ service, setTreatment }) => {
     const { name, slots } = service;
     return (
         <div class="card lg:max-w-lg bg-base-100 shadow-xl">
-            <div class="card-body">
-                <h2 class="card-title text-secondary">{name}</h2>
+            <div class="card-body text-center">
+                <h2 class="text-secondary text-xl font-bold">{name}</h2>
                 <p>
                     {
                         slots.length > 0
@@ -16,7 +16,7 @@ const Service = ({ service, setTreatment }) => {
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'}  available</p>
                 <div class="card-actions justify-center">
                     <label disabled={slots.length === 0}
-                        onClick={() => setTreatment(service)} for="booking-modal" class="btn btn-secondary text-white uppercase">Book Appointment</label>
+                        onClick={() => setTreatment(service)} for="booking-modal" class="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
                 </div>
             </div>
         </div>
